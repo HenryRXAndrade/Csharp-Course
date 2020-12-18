833db23a1dad28055cfb6a2766ccdcfaf34c72f1
Henry Andrade <henry.andrade@outlook.com>
2020-12-18 14:41:24 -0300

    Exercício proposto

---
+++ ExeProp3/Entities/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExeProp3.Entities.Exception;
+
+namespace ExeProp3.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > WithdrawLimit)
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+            if (Balance < amount)
+            {
+                throw new DomainException("Not enough balance");
+            }
+
+            Balance -= amount;
+
+        }
+
+
+
+    }
+}
---
+++ ExeProp3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using ExeProp3.Entities.Exception;
+using ExeProp3.Entities;
+
+namespace ExeProp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter account data:");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account acc = new Account(number, holder, initialBalance, withdrawLimit);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            try
+            {
+                acc.Withdraw(amount);
+                Console.Write("New balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine();
+
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+
+
+        }
+    }
+}
